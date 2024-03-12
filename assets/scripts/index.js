0b5828a2aafd148a0fe4b1e42f5cebf638346dc0
Talgat <retalgat@gmail.com>
2024-03-12 10:03:42 +0500

    Updates

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,1 +1,1 @@
-"use strict";
+"use strict";const header=document.querySelector("header"),scrollChange=1;window.addEventListener("scroll",(function(){window.scrollY>=1?header.classList.add("header--scroll"):header.classList.remove("header--scroll")}));
